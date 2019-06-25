d66bdfb2c98029a54c43478c1fe9c2e9b439b755
jackstrosahl <strosahljack@gmail.com>
2019-06-25 12:42:55 -0500

    Restore

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my personal(professional) site.  It's built on gatsby.
---
+++ src/components/Head.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Helmet} from "react-helmet";
+
+class Head extends React.Component {
+  render () {
+    return (
+        <div>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Jack Strosahl</title>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"></link>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous"></link>
+                <link href="https://fonts.googleapis.com/css?family=Noto+Sans+HK|Source+Code+Pro&display=swap" rel="stylesheet"></link>
+
+                <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="crossorigin="anonymous"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+                <script src="https://kit.fontawesome.com/0322e445d9.js"></script>
+            </Helmet>
+        </div>
+    );
+  }
+};
+
+export default Head;
