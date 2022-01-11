7985b0006efd640cc0cead8a4b1aa7d6b541f471
jackstrosahl <strosahljack@gmail.com>
2022-01-10 21:02:37 -0600

    Add credit

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,7 @@ class IndexPage extends React.Component
     <div>
       <Head/>
       <App data={this.data.data}/>
+      <p style={{position:"absolute", bottom:"1px", left:"1px"}}>By <a href="//commons.wikimedia.org/w/index.php?title=User:Av9&amp;amp;action=edit&amp;amp;redlink=1" class="new" title="User:Av9 (page does not exist)">Av9</a> - <span class="int-own-work" lang="en">Own work</span>, <a href="https://creativecommons.org/licenses/by-sa/4.0" title="Creative Commons Attribution-Share Alike 4.0">CC BY-SA 4.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=74777664">Link</a></p>
     </div>);
   }
 }
