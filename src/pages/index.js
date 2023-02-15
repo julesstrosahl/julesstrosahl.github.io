72154cb1a9108b79811d7297f37680ac6ad65733
jackstrosahl <strosahljack@gmail.com>
2023-02-14 17:49:25 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,15 @@ class IndexPage extends React.Component
   {
     super();
 
-    this.data = {data};
+    this.data = data;
   }
   render()
   {
   return(
     <div>
       <Head/>
-      <App data={this.data.data}/>
-      <p style={{position:"absolute", bottom:"1px", left:"1px",fontSize:"12px"}}>Image By <a href="//commons.wikimedia.org/w/index.php?title=User:Av9&amp;amp;action=edit&amp;amp;redlink=1" class="new" title="User:Av9 (page does not exist)">Av9</a> - <span class="int-own-work" lang="en">Own work</span>, <a href="https://creativecommons.org/licenses/by-sa/4.0" title="Creative Commons Attribution-Share Alike 4.0">CC BY-SA 4.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=74777664">Link</a></p>
+      <App data={this.data}/>
+      <p style={{position:"absolute", bottom:"1px", left:"1px",fontSize:"12px"}}>Image By <a href="//commons.wikimedia.org/w/index.php?title=User:Av9&amp;amp;action=edit&amp;amp;redlink=1" className="new" title="User:Av9 (page does not exist)">Av9</a> - <span className="int-own-work" lang="en">Own work</span>, <a href="https://creativecommons.org/licenses/by-sa/4.0" title="Creative Commons Attribution-Share Alike 4.0">CC BY-SA 4.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=74777664">Link</a></p>
     </div>);
   }
 }
